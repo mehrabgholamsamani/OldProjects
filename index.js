fc177ecd7f1a5e675e1c355c95c2273766d6fb9a
Mehrab Gholamsamani <mehrabgholamsamani@gmail.com>
2026-01-20 19:17:22 +0100

    changes

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ const stocks = {
     holder: ["cone" , "cup"],
     toppings: ["choclate" , "sprinkles" , "fruits"]
 }
+
+function stepOne(){
+    return new Promise(function(resolve,reject){
+        let isAvailable = true;
+        if(isAvailable){
+            setTimeout(() => {resolve("your ice cream flavor is " + stocks.flavor[0])} , 2000);
+        }else{
+            reject("we dont have this flavor");
+        }
+    })
+}
+
+function stepTwo(flavored){
+    return new Promise(function(resolve , reject){
+        let isAvailable = true;
+        if(isAvailable){
+            setTimeout( () => {resolve(flavored + " and its in a " + stocks.holder[1])} , 1000)
+        }else{
+            reject("we dont have this holder");
+        }
+    })
+}
