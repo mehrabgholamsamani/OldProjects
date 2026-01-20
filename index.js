4625a234f71fef8eb97157f5f66c823ba3c0a464
Mehrab Gholamsamani <mehrabgholamsamani@gmail.com>
2026-01-20 19:16:44 +0100

    changes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 /// this is the first tiny project i made when first time learning async,
+
+const stocks = {
+    flavor: ["strawberry" , "banana" , "apple"],
+    holder: ["cone" , "cup"],
+    toppings: ["choclate" , "sprinkles" , "fruits"]
+}
